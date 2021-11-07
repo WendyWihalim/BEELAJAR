d6d9507e2b3b22cc95f8580ef1638252b421f3c8
WendyWihalim <wendy.wihalim@binus.ac.id>
2021-11-07 15:27:28 +0700

    BEELAJAR

---
+++ script.js
@@ -0,0 +1,8 @@
+var audio = new Audio();
+audio.src = "audio/bell.mp3"
+
+function validationForm() {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+}
